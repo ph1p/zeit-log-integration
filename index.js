94c5432638e22550f8e886b245f3ffba36adf882
ph1p <phil@capshake.com>
2019-06-01 16:46:50 +0200

    feat(icon): add icon endpoint
    show more details

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { withUiHook, htm } = require('@zeit/integration-utils');
 
+const HOST = 'http://localhost:5005';
+
 const apiClient = zeitClient => ({
   async getDeployments(projectId) {
     return await zeitClient.fetchAndThrow(
@@ -26,36 +28,51 @@ const apiClient = zeitClient => ({
   }
 });
 
-let deployments = [];
-let builds = [];
-let buildLogs = [];
-let outputLogs = [];
-let otherLogs = [];
-let deployment = null;
-
 const transformLogLine = text => {
   if (text.includes('-')) {
   }
-  let t = text.split('\n');
+  let newText = text.replace(/\033\[[0-9;]*m/g, '');
+  console.log(newText);
 
-  return t.length
-    ? t.map(te => htm`${te.replace(/\033\[[0-9;]*m/g, '')}<BR />`)
+  if (newText.includes('REPORT RequestId:') === newText.length - 2) {
+    return newText;
+  }
+
+  newText = newText.split('\n');
+  return newText.length
+    ? newText.map(te => htm`${te}<BR />`)
     : text;
 };
 
-const Log = ({ logs }) => {
+const getIconByFile = (file, width = 40, height = 40) => {
+  const ext = file.split('.');
+  if (ext.length > 1) {
+    return htm`<Box display="inline-block" backgroundImage=${'url(' +
+      HOST +
+      '/' +
+      ext.pop() +
+      '.svg)'} width=${width + 'px'} height=${height +
+      'px'} backgroundSize="cover" />`;
+  }
+  return '';
+};
+
+const Log = ({ logs, name }) => {
   return logs.length
     ? htm`<Fieldset>
     <FsContent>
-      <Box height="400px" overflow="auto">
+      ${getIconByFile(name)}
+      <FsTitle>${name}</FsTitle>
+      <Box height="400px" overflow="auto" lineHeight="20px" backgroundColor="#000" color="#fff" padding="20px" borderRadius="5px">
         ${logs.map(log => {
           const { info, text } = log.payload;
           const { type, entrypoint, path, name } = info;
 
-          return htm`<Box display="grid" gridTemplateColumns="200px 1fr">
-            <Box>${type} ${entrypoint}</Box>
-            <Box>${transformLogLine(text)}</Box>
-          </Box>`;
+          if (type === 'output') {
+            return htm`<Box>${transformLogLine(text)}</Box>`;
+          }
+
+          return htm` <Box>${transformLogLine(text)}</Box>`;
         })}
       </Box>
     </FsContent>
@@ -63,20 +80,67 @@ const Log = ({ logs }) => {
     : '';
 };
 
+const getLocationList = out => {
+  if (out.lambda && out.lambda.deployedTo) {
+    return htm`<Box><B>Locations:</B> ${out.lambda.deployedTo.map(lo => {
+      const name = lo.replace(/[0-9]/g, '');
+
+      return htm`<Link target="_blank" href=${'https://' +
+        name +
+        '.zeit.co'}>${lo.toUpperCase()}</Link>`;
+    })}</Box>`;
+  }
+  return '';
+};
+
+const allLogs = {
+  output: [],
+  others: []
+};
+
 module.exports = withUiHook(async ({ payload, zeitClient }) => {
   const api = apiClient(zeitClient);
+  const metadata = await zeitClient.getMetadata();
+
+  // vars
+  let deployments = [];
+  let builds = [];
+  let buildLogs = [];
+  let outputLogs = [];
+  let otherLogs = [];
+  let deployment = null;
 
   if (payload.projectId) {
-    deployments = (await api.getDeployments(payload.projectId)).deployments;
+    deployments = await api.getDeployments(payload.projectId);
   }
 
   if (payload.action === 'change-deployment') {
     const { selectedDeployment } = payload.clientState;
 
+    metadata.selectedDeployment = selectedDeployment;
+    await zeitClient.setMetadata(metadata);
+
     builds = (await api.getDeploymentBuilds(selectedDeployment)).builds || [];
-    deployment = (await api.getDeploymentById(selectedDeployment)).builds || [];
+    deployment = (await api.getDeploymentById(selectedDeployment)) || [];
 
     const logs = await api.getDeploymentLogs(selectedDeployment);
+
+    logs.forEach(l => {
+      if (l.payload.info.entrypoint && !allLogs[l.payload.info.entrypoint]) {
+        allLogs[l.payload.info.entrypoint] = [];
+      }
+
+      if (l.payload.info.type === 'output') {
+        allLogs['output'].push(l);
+      } else if (typeof l.payload.info.entrypoint === 'undefined') {
+        allLogs['others'].push(l);
+      } else {
+        allLogs[l.payload.info.entrypoint].push(l);
+      }
+    });
+
+    console.log(Object.keys(allLogs));
+
     buildLogs = logs.filter(
       l => l.type === 'stdout' && l.payload.info.type === 'build'
     );
@@ -93,38 +157,85 @@ module.exports = withUiHook(async ({ payload, zeitClient }) => {
 
   return htm`
     <Page>
-
       <Fieldset>
         <FsContent>
           <Box display="grid" gridTemplateColumns="1fr 1fr" alignItems="center">
             <ProjectSwitcher />
-            <Select name="selectedDeployment" value="selectedDeployment" action="change-deployment">
-              <Option value="" caption="Please select..." />
-              ${deployments.map(
-                deployment =>
-                  htm`<Option value=${deployment.uid} caption=${
-                    deployment.url
-                  } />`
-              )}
-            </Select>
+
+            ${
+              deployments.deployments
+                ? htm`
+                  <Select name="selectedDeployment" value="selectedDeployment" action="change-deployment">
+                    <Option value="" caption="Select deployment..." />
+                    ${deployments.deployments.map(deployment => {
+                      let name = deployment.url;
+
+                      if (deployment.state === 'ERROR') {
+                        name = 'Error -> ' + name;
+                      }
+
+                      return htm`<Option value=${
+                        deployment.uid
+                      } caption=${name} />`;
+                    })}
+                  </Select>`
+                : ''
+            }
           </Box>
         </FsContent>
       </Fieldset>
 
-${JSON.stringify(deployment)}
+      ${
+        deployment
+          ? htm`
+          <H2>${deployment.name}</H2>
+          <Box display="grid" gridTemplateColumns="1fr 1fr" gridGap="20px">
 
-      <Box>
-        ${builds.map(build => htm`<Box>${JSON.stringify(build)}</Box>`)}
-      </Box>
+            <Box>
+              <Fieldset>
+                <FsContent>
+                  <UL>
+                    ${deployment.build.env.map(e => htm`<LI>${e}</LI>`)}
+                  </UL>
+                </FsContent>
+              </Fieldset>
+            </Box>
+
+            <Box height="400px" overflow="auto">
+              <Fieldset>
+                <FsContent>
+                  <FsTitle>Builds</FsTitle>
+                  ${builds.map(
+                    build => htm`<Box display="grid" gridGap="10px">
+                    ID: ${build.id}
+                    ${build.output.map(
+                      out => htm`<Box backgroundColor="#f3f3f3" borderRadius="5px" display="grid" gridTemplateColumns="20px 1fr 70px" position="relative" padding="10px" fontSize="12px">
+                        <Box>${getIconByFile(out.path, 17, 15)}</Box>
+                        <Box>${out.path}</Box>
+                        <Box textAlign="right" color="#999">${(
+                          out.size /
+                          1024 /
+                          1024
+                        ).toFixed(2)} MB</Box>
+                        ${getLocationList(out)}
+                      </Box>`
+                    )}</Box>`
+                  )}
+                </FsContent>
+              </Fieldset>
+            </Box>
+          </Box>
+
+          <Box display="grid" gridTemplateColumns="1fr" gridGap="20px" marginTop="20px">
+            ${Object.keys(allLogs).map(
+              l => htm`<Box><${Log} name=${l} logs=${allLogs[l]} /></Box>`
+            )}
+          </Bo>
 
 
-
-
-
-
-      <${Log} logs=${buildLogs} />
-      <${Log} logs=${outputLogs} />
-      <${Log} logs=${otherLogs} />
+      `
+          : ''
+      }
 
 		</Page>
 	`;
